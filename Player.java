a0b4e13c9f0d72de32c10040501685f21cc5ee8f
TrysoftCoding <134542572+TrysoftCoding@users.noreply.github.com>
2023-06-01 13:44:31 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,7 +2,6 @@ import java.awt.*;
 public class Player{
 	private final static int LEFT = 65,RIGHT = 68,JUMP = 87,ABILITY = 32;
 	private int x,y,w,h,vx,vy,jp;
-	private Sword sword;
 	public Player(int xx,int yy,int ww,int hh,int vvx, int vvy, int jjpp){
 		x = xx;
 		y = yy;
@@ -11,35 +10,40 @@ public class Player{
 		vx = vvx;
 		vy = vvy;
 		jp = jjpp;
-		sword = null;
 	}
 	
 	public void move(boolean []keys){	//moves player
-		if(keys[BaseFrame.A] && x>=0){
+		if(keys[LEFT] && x>=0){
 			x-=vx;
 		}
-		if(keys[BaseFrame.D] && x <= BaseFrame.WIDTH-w){
+		if(keys[RIGHT] && x<=Game.WIDTH-w){
 			x+=vx;
 		}
-		if(keys[BaseFrame.W] && y+h >= BaseFrame.HEIGHT){
+		if(keys[JUMP] && y+h >= Game.HEIGHT){
 			vy -= jp;
 		}
 		y += vy;
 		vy += BaseFrame.GRAVITY;
-		if(y+h >= BaseFrame.HEIGHT){
+		if(y+h >= Game.HEIGHT){
 			vy = 0;
-			y = BaseFrame.HEIGHT-h;
+			y = Game.HEIGHT-h;
 		}
-		if(keys[BaseFrame.SPACE]){
-			sword = new Sword(x,y);
-			sword.strike();
+		int platx, platy, platlen, platbot, playerbot;
+		platx = Game.plat1.getx();
+		platy = Game.plat1.gety();
+		platlen = Game.plat1.getLength();
+		if(x <= platx + platlen && x + w >= platx){
+			if(y+h+vy >= platy && y+h <= platy){
+				vy = 0;
+				y = platy - h;
+				if(keys[JUMP]){vy -= jp;}
+			}
 		}
 	}
 	
 	public void draw(Graphics g){	//draws player
 		g.setColor(Color.RED);
 		g.fillRect(getX(),getY(),getW(),getH());
-//		sword.draw(g);
 	}    
 	
 	public int getX(){return x;}
