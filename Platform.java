40ec2cb2f42b261866145a70bf57aab6e4646fb3
TrysoftCoding <134542572+TrysoftCoding@users.noreply.github.com>
2023-06-02 13:42:44 -0400

    Update Platform.java

--- Platform.java
+++ Platform.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import javax.swing.*;
 
 class Platform{
-    private int x,y,w,h;
+    private int x,y,w,h,offsetX,offsetY;
     Image plat;
 
     public Platform(int xx, int yy, int ww, int hh){
@@ -10,15 +10,18 @@ class Platform{
         y = yy;
         w = ww;
         h = hh;
+        offsetY = y+10;
         plat = new ImageIcon("PlatImage.png").getImage();
     }
 
     public void draw(Graphics g){
-        g.drawImage(plat,x,y,null);
+        Graphics2D g2d = (Graphics2D)g;
+        offsetX = x - Game.player.getRelX();
+        g2d.drawImage(plat,offsetX,y,null);
     }
 
-    public int getX(){return x;}
-    public int getY(){return y;}
+    public int getX(){return offsetX;}
+    public int getY(){return offsetY;}
     public int getW(){return w;}
     public int getH(){return h;}
 }
