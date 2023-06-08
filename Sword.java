d61e23581a9d4481d110860ed4a9df0f870c0a10
BGen-T-E-Snow <134542037+BGen-T-E-Snow@users.noreply.github.com>
2023-06-08 14:26:03 -0400

    Update Sword.java

--- Sword.java
+++ Sword.java
@@ -3,14 +3,22 @@ public class Sword{
 	private int x,y,w,h;
 	private Rectangle swordRect;
 	
-    public Sword(int xx, int yy) {
+    public Sword(int xx, int yy, int ww, int hh) {
     	x = xx;
     	y = yy;
+		w = ww;
+		h = hh;
 		swordRect = null;
     }
     
-    public void strike(boolean isRight){
-		swordRect = Game.getPlayer().getPlayerRect();
+    public void strike(boolean isRight){//
+		int pW = Game.getPlayer().getIdleW();
+		if(isRight){
+			swordRect = new Rectangle(x+pW,y,w,h);
+		}
+		else{
+			swordRect = new Rectangle(x,y,w,h);
+		}
     }
 	
     public void draw(Graphics g){
