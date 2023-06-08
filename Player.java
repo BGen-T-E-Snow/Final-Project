fe94a64839c6764be4f09420b99a3b4fda3a1549
BGen-T-E-Snow <134542037+BGen-T-E-Snow@users.noreply.github.com>
2023-06-08 14:24:59 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,9 +3,9 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Player{
-	private final static int LEFT = Game.A ,RIGHT = Game.D, JUMP = Game.W , STRIKE = Game.SPACE, RJUMP = 0, LJUMP = 1, RFALL = 2, LFALL = 3, RSTRIKE = 4, LSTRIKE = 5, RRUN = 6, LRUN = 7, RIDLE = 8, LIDLE = 9, RIGHTEDGE = 650, LEFTEDGE = 150;
+	private final static int LEFT = Game.A ,RIGHT = Game.D, JUMP = Game.W , STRIKE = Game.SPACE, RJUMP = 0, LJUMP = 1, RFALL = 2, LFALL = 3, RSTRIKE = 4, LSTRIKE = 5, RRUN = 6, LRUN = 7, RIDLE = 8, LIDLE = 9, RDEATH = 10, LDEATH = 11, RIGHTEDGE = 650, LEFTEDGE = 150;
 	private int x,y,w,h,vx,vy,jp,health,relX;
-	private boolean isRight, jumping, falling, striking, idling, running;
+	private boolean isRight, jumping, falling, striking, idling, running, death;//
 	private int idleW,idleH;
 	private double col;
 	private int row;
@@ -35,6 +35,8 @@ public class Player{
 		pics.add(addPics("RunLeft",8));
 		pics.add(addPics("IdleRight",8));
 		pics.add(addPics("IdleLeft",8));
+		pics.add(addPics("DeathRight",6));
+		pics.add(addPics("DeathLeft",6));
 		isRight = true;
 		jumping = false;
 		falling = false;
@@ -50,29 +52,36 @@ public class Player{
 	}
 	
 	public void move(boolean []keys){	//moves player	
-		if(keys[LEFT]){
+		if(health<=0){//
+			death = true;
+		}
+		if(keys[STRIKE] && !striking){//
+			striking = true;
+			col = 0;
+		}
+		else if(keys[LEFT]){
 			isRight = false;
 			running = true;
-			if(x>=LEFTEDGE){
-				x-=vx;
-			}
-			else if(relX > 0){
-				relX-=vx;
+			if(!striking){
+				if(x>=LEFTEDGE){//
+					x-=vx;
+				}
+				else if(relX > 0){//
+					relX-=vx;
+				}
 			}
 		}
 		else if(keys[RIGHT]){
 			isRight = true;
 			running = true;
-			if(x<=RIGHTEDGE-w){
-				x+=vx;
+			if(!striking){
+				if(x<=RIGHTEDGE-w){//
+					x+=vx;
+				}
+				else{//
+					relX+=vx;
+				}
 			}
-			else{
-				relX+=vx;
-			}
-		}
-		else if(keys[STRIKE] && !jumping && !falling && !striking){
-			striking = true;
-			col = 0;
 		}
 		else{
 			idling = true;
@@ -114,9 +123,13 @@ public class Player{
 			jumping = false;
 			falling = true;
 		}
-		if(striking){//
+		if(death){//
+			row = isRight ? RDEATH:LDEATH;
+		}
+		else if(striking){//
+			h = image.getHeight(null);//
 			sword = new Sword(x,y);
-			sword.strike(isRight);
+			sword.strike(isRight);//
 			row = isRight ? RSTRIKE:LSTRIKE;
 		}
 		else if(jumping){//
@@ -131,45 +144,49 @@ public class Player{
 		else if(idling){//
 			row = isRight ? RIDLE:LIDLE;
 		}
-		if((row==RJUMP || row==LJUMP || row==RFALL || row==LFALL) && col>=1){//
-			col = 0;
-		}
 		if((row==RSTRIKE || row==LSTRIKE) && col>=3){//
 			col = 0;
 			striking = false;
 			sword = null;
 		}
-		if((row==RIDLE || row==LIDLE || row==RRUN || row==LRUN) && col>=7){//
+		else if((row==RJUMP || row==LJUMP || row==RFALL || row==LFALL) && col>=1){//
 			col = 0;
+		}
+		else if((row==RIDLE || row==LIDLE || row==RRUN || row==LRUN) && col>=7){//
+			col = 0;
+		}
+		else if((row==RDEATH || row==LDEATH) && col>=5){//
+			col = 5;
+			Game.setScreen(Game.DEATH);
 		}
 		col += 0.2;
 	}
 		
 	public void draw(Graphics g){	//draws player
+		if(sword!=null){				//uncomment if you wanna draw the swords hitbox
+			g.setColor(Color.RED);
+			sword.draw(g);
+		}
 		g.setColor(Color.YELLOW);
 		image = pics.get(row).get((int)col);
-		w = image.getWidth(null);
+		w = striking ? idleW:image.getWidth(null);
 		if(row!=LSTRIKE){//
 			g.fillRect(x,y,w,h);
 			playerRect = new Rectangle(x,y,w,h);//
 			g.drawImage(image,x,y,null);
 		}
 		else{//
-			g.fillRect(x-w+idleW,y,w,h);//
+			int xVal = striking ? x+w+idleW:x-w+idleW;
+			g.fillRect(xVal,y,w,h);//
 			playerRect = new Rectangle(x-w+idleW,y,w,h);//
 			g.drawImage(image,x-w+idleW,y,null);//
 		}//
-//		if(sword!=null){				//uncomment if you wanna draw the swords hitbox
-//			g.setColor(Color.RED);
-//			sword.draw(g);
-//		}
 	}    
 	
 	public ArrayList<Image> addPics(String name,int end){
 		ArrayList<Image> picType = new ArrayList<Image>();
 		for(int i=1; i<=end; i++){
 			picType.add(new ImageIcon(String.format("Player Images/%s%03d.png",name,i)).getImage());
-			System.out.println(String.format("added Player Images/%s%03d.png",name,i));
 		}
 		return picType;
 	}
