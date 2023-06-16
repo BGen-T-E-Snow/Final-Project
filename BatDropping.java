51d28aa999e8dc426dda65deb158990ca3b73ffb
BGen-T-E-Snow <134542037+BGen-T-E-Snow@users.noreply.github.com>
2023-06-16 18:49:59 -0400

    Update BatDropping.java

--- BatDropping.java
+++ BatDropping.java
@@ -1,16 +1,14 @@
 import java.awt.*;
+
 import javax.swing.ImageIcon;
 
-//Exists to reduce the amount of code in Bat.java and make it more convinient to create bat poop. Has functions to
-//move and draw itself. Damages player upon contact.
 class BatDropping{
     private int x,y,vy,w,h;
-    private boolean hitGround; //If it has hit the ground yet
+    private boolean hitGround;
     Rectangle hitbox;
     Image poop;
     private int offsetX;
 
-    //Returns nothing and has parameters for the x and y coordinates. Creates a new BatDropping.
     public BatDropping(int xx, int yy){
         x = xx;
         y = yy;
@@ -24,29 +22,27 @@ class BatDropping{
         poop = new ImageIcon("poop.png").getImage();
     }
 
-    //Returns nothing and no parameters. Makes the poop fall as though there were gravity.
-    public void move(){
+    public void move(Player player){
         y += vy;
 		vy += Game.GRAVITY;
-        if(y+h >= Game.HEIGHT){ //Detects when it has hit the ground
+        if(y+h >= Game.HEIGHT){
             vy = 0;
 			y = Game.HEIGHT-h;
             hitGround = true;
         }
         offsetX = x - Game.getPlayer().getRelX();
-        hitbox.x = offsetX; //Changes the coordinates in hitbox correspondingly
+        hitbox.x = offsetX;
         hitbox.y = y;
     }
 
-    //Returns nothing and has a Graphics parameter to draw things
     public void draw(Graphics g){
         offsetX = x - Game.getPlayer().getRelX();
-        g.fillRect(offsetX,y,w,h);
+//        g.fillRect(offsetX,y,w,h);
         g.drawImage(poop, offsetX, y, null);
     }
 
-    public int getX(){return offsetX;} //Returns the x coordinate of poop and no parameters
-    public int getY(){return y;} //Returns the y and no parameters
-    public boolean hitGround(){return hitGround;} //Returns true if it has hit the ground. No parameters.
-    public Rectangle getRect(){return hitbox;} //Returns the hitbox of the poop and no parameters
+    public int getX(){return offsetX;}
+    public int getY(){return y;}
+    public boolean hitGround(){return hitGround;}
+    public Rectangle getRect(){return hitbox;}
 }
