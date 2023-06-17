0b25fc5c1614faec1b0d51b9bcea2eaf99942195
BGen-T-E-Snow <134542037+BGen-T-E-Snow@users.noreply.github.com>
2023-06-16 23:57:36 -0400

    Update Sword.java

--- Sword.java
+++ Sword.java
@@ -1,3 +1,7 @@
+//Sword.java
+//Spencer Trepanier
+//Defines the sword
+//has a strike and a hit method, and an x,y, width and height, as well as a damagebox
 import java.awt.*;
 public class Sword{
 	private int x,y,w,h;
@@ -8,24 +12,19 @@ public class Sword{
     	y = yy;
 		w = ww;
 		h = hh;
-		swordRect = new Rectangle(x,y,w,h);
+		swordRect = new Rectangle(x,y,w,h);	//damagebox
     }
     
-    public void strike(boolean isRight){//
-		int pW = Game.getPlayer().getIdleW();
-		if(isRight){
-			swordRect = new Rectangle(x+pW,y,w,h);
-		}
-		else{
-			swordRect = new Rectangle(x-w,y,w,h);
-		}
+    public void strike(boolean isRight, Player player){	//determines the sword rectangle based on which direction player is facing
+		int pW = player.getIdleW();		//gets the players idle width
+		swordRect = isRight ? new Rectangle(x+pW,y,w,h):new Rectangle(x-w,y,w,h); //determines which side of player to put the sword based on the direction
     }
 	
-	public boolean swordHit(Rectangle enemyRect){
+	public boolean swordHit(Rectangle enemyRect){	//checks if sword is colliding with given rectangle
 		return swordRect.intersects(enemyRect);
 	}
 	
-    public void draw(Graphics g){
+    public void draw(Graphics g){	//draws sword (optional)
 		g.setColor(Color.RED);
 		g.fillRect((int)swordRect.getX(),(int)swordRect.getY(),(int)swordRect.getWidth(),(int)swordRect.getHeight());
     }    
