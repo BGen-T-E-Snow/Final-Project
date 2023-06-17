484162d774dc5d1961c05e762bedfb9f4f93e3e9
TrysoftCoding <134542572+TrysoftCoding@users.noreply.github.com>
2023-06-16 22:20:11 -0400

    Update Platform.java

--- Platform.java
+++ Platform.java
@@ -1,27 +1,31 @@
 import java.awt.*;
 import javax.swing.*;
 
+//Exists to make creating platforms a lot easier. Takes in sets of variables and creates a platform in the position given
 class Platform{
-    private int x,y,w,h,offsetX,offsetY;
+    private int x,y,w,h,offsetX,offsetY; //OffsetY is used to compensate for the imperfect image so it doesn't look like
+    //the player is floating slightly above the platform
     Image plat;
 
+    //Returns nothing and has parameters for the location and sixe of the platform. Creates the platform.
     public Platform(int xx, int yy, int ww, int hh){
         x = xx;
         y = yy;
         w = ww;
         h = hh;
-        offsetY = y+3;//
+        offsetY = y+3;
         plat = new ImageIcon("PlatImage.png").getImage();
     }
 
+    //Returns nothing and has a Graphics parameter used to draw. Draws the platform.
     public void draw(Graphics g){
         Graphics2D g2d = (Graphics2D)g;
-        offsetX = x - Game.player.getRelX();
+        offsetX = x - Game.getPlayer().getRelX();
         g2d.drawImage(plat,offsetX,y,null);
     }
 
-    public int getX(){return offsetX;}
-    public int getY(){return offsetY;}
-    public int getW(){return w;}
-    public int getH(){return h;}
+    public int getX(){return offsetX;} //Returns the offsetX for the platform and no parameters
+    public int getY(){return offsetY;} //Returns the offsetY for the platform and no parameters
+    public int getW(){return w;} //Returns the width of the platform and no parameters
+    public int getH(){return h;} //Returns the height of the platform and no parameters
 }
